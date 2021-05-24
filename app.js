f9d245f4595b739205caed93bad7b62373ffcebc
Dominic Frempong <dafrempong@gmail.com>
2021-05-23 19:40:45 -0700

    fix: updating connetion to the backend

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const port = process.env.PORT || 8080
 const app = express()
 
+const apiProxy = createProxyMiddleware('/api', { target: 'http://localhost:4000' })
+app.use(apiProxy)
+
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'static/index.html'))
 })
+
+app.use('/api/*', express.static(path.join(__dirname, 'server')))
 
 app.listen(port)
--- server/routes/recipe.ts
+++ server/routes/recipe.ts
@@ -1,5 +1,7 @@
 import { RecipeModel } from "../models"
 import { Request, Response, NextFunction } from "express"
+import  mongoose from 'mongoose'
+
 
 const formatIngredients = (ingredient) : {id:string, name:string[], unit:string, amount: string } => {
   const {id, name, unit, amount} = ingredient
@@ -15,10 +17,11 @@ const formatRecipe = (recipe) : {id:string, ingredients:string[], name:string, i
 export const recipeMiddleware = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
   // TODO fetch and return a recipe
   const {id} = req.params
-  const foundRecipes = await RecipeModel.findOne({_id:id})
-  if (!foundRecipes) {
-    next()
-  } else {
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const foundRecipes = await RecipeModel.findOne({_id:id})
     res.send(formatRecipe(foundRecipes))
+  } else {
+    res.status(404).send({message:`Could not find recipe with provided id:${id}`})
   }
+
 }
